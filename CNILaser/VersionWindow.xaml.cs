931f918825aaf826a6fb85f159001409c5d6cf1c
YZ <1124955915@qq.com>
2026-02-10 16:11:03 +0800

    fix：版本信息改成英文

--- CNILaser/VersionWindow.xaml.cs
+++ CNILaser/VersionWindow.xaml.cs
@@ -12,19 +12,15 @@ namespace Simscop.Spindisk.Wpf.Views
         {
             InitializeComponent();
 
-            Assembly assembly = Assembly.GetExecutingAssembly();
+            var assembly = Assembly.GetExecutingAssembly();
 
-            string assemblyVersion = assembly.GetName().Version?.ToString() ?? "未知";
-            string fileVersion = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version ?? "未知";
-            string infoVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "未知";
-            string name = assembly.GetName().Name ?? "未知程序集";// 名称
-            string company = assembly.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company ?? "未知公司";// 公司
-            string product = assembly.GetCustomAttribute<AssemblyProductAttribute>()?.Product ?? "未知产品"; // 产品
-            string title = assembly.GetCustomAttribute<AssemblyTitleAttribute>()?.Title ?? "未知标题";// 标题
+            // 读取 FileVersion
+            var fileVersion = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
+            VersionText.Text = $"Version: {fileVersion}";
 
-            SoftwareName.Text = title;
-            CompanyText.Text = $"公司：{company}";
-            VersionText.Text = $"版本号：v{fileVersion}";
+            // 读取 Company
+            var company = assembly.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company;
+            CompanyText.Text = company;
         }
 
         protected override void OnClosing(CancelEventArgs e)
